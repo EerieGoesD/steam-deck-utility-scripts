a3c599b4971d93703d303281be4095308c7784b7
EERIE <65854026+EerieGoesD@users.noreply.github.com>
2026-03-28 17:55:29 +0000

    Update remove_roms_metadata.sh

--- scripts/remove_roms_metadata.sh
+++ scripts/remove_roms_metadata.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -rf /run/media/deck/EmuDeck/Emulation/tools/downloaded_media/
 find ~/.config/EmuDeck/backend/configs/emulationstation/gamelists/ -name "gamelist.xml" -delete
+find ~/ES-DE/gamelists/ -name "gamelist.xml" -delete
 echo "Done. Media and metadata cleared."
 read -p "Press Enter to close..."
